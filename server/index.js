8c1f9e91bd622055c764576b680d8ca04cfcc2d8
Muhammad Reza Zulman <m.rezazulman@gmail.com>
2024-10-18 16:40:32 +0700

    project initialization

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.use("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello World",
+  });
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server started, listening on port ${PORT}`);
+});
